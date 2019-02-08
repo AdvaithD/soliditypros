b8c20c094ebb44730635f795b8c00b2493850fcb
Tanner Linsley <tannerlinsley@gmail.com>
2019-02-08 14:18:08 -0700

    Remove demo posts

--- static.config.js
+++ static.config.js
@@ -1,28 +1,5 @@
-import axios from "axios";
+// import axios from "axios";
 
 export default {
-  plugins: ["react-static-plugin-mdx", "react-static-plugin-styled-components"],
-  getSiteData: () => ({
-    title: "React Static"
-  }),
-  getRoutes: async () => {
-    const { data: posts } = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
-    return [
-      {
-        path: "/blog",
-        getData: () => ({
-          posts
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          component: "src/containers/Post",
-          getData: () => ({
-            post
-          })
-        }))
-      }
-    ];
-  }
+  plugins: ["react-static-plugin-mdx", "react-static-plugin-styled-components"]
 };
