a7dc64fd647eb55d9edbf799ee14cfd88fbedd1b
Tanner Linsley <tannerlinsley@gmail.com>
2019-02-08 13:48:45 -0700

    Update React Static

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,30 +1,33 @@
 import { useState } from "react";
 
 const useLocalStorage = (key, initial) => {
-  let local;
-  try {
-    local = JSON.parse(localStorage.getItem(key));
-  } catch (err) {
-    //
+  if (typeof document !== "undefined") {
+    let local;
+    try {
+      local = JSON.parse(localStorage.getItem(key));
+    } catch (err) {
+      //
+    }
+    if (!local) {
+      localStorage.setItem(key, JSON.stringify(initial));
+      local = initial;
+    }
+    const [state, _setState] = useState(local);
+    const setState = updater => {
+      _setState(old => {
+        let res;
+        if (typeof updater === "function") {
+          res = updater(old);
+        } else {
+          res = updater;
+        }
+        localStorage.setItem(key, JSON.stringify(res));
+        return res;
+      });
+    };
+    return [state, setState];
   }
-  if (!local) {
-    localStorage.setItem(key, JSON.stringify(initial));
-    local = initial;
-  }
-  const [state, _setState] = useState(local);
-  const setState = updater => {
-    _setState(old => {
-      let res;
-      if (typeof updater === "function") {
-        res = updater(old);
-      } else {
-        res = updater;
-      }
-      localStorage.setItem(key, JSON.stringify(res));
-      return res;
-    });
-  };
-  return [state, setState];
+  return [initial, () => {}];
 };
 
 export default useLocalStorage;
